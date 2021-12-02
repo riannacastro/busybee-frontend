81f37b1cb7f573883501324ea221e65b352e5173
Rianna Castro <riannaxcastro@gmail.com>
2021-12-02 09:20:32 -1000

    navbar showing

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function NavBar() {
     return (
         <div>
-            
+            <button>Home</button>
+            <button>Important Tasks</button>
+            <button>Finished Tasks</button>
         </div>
     )
 }
