60f2a67eb8c0e1c40192595b9840d229050cfe65
Rianna Castro <riannaxcastro@gmail.com>
2021-12-02 09:22:27 -1000

    pages working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import Pages from './components/Pages';
 
 function App() {
   return (
     <div className="App">
       < NavBar />
+      < Pages />
     </div>
   );
 }
