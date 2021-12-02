cd754ec89635f2d4c7e033e575967b02cb872816
Rianna Castro <riannaxcastro@gmail.com>
2021-12-02 09:59:49 -1000

    navbar working

--- src/components/Pages.js
+++ src/components/Pages.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import HomeContainer from './home/HomeContainer'
+import ImportantContainer from './important tasks/ImportantContainer'
+import FinishedContainer from './finished tasks/FinishedContainer'
 
 export default function Pages({page}) {
+    function renderPage() {
+        switch (page){
+            case "home":
+                return < HomeContainer />
+
+            case "important tasks":
+                return < ImportantContainer />
+            
+            case "finished tasks":
+                return < FinishedContainer />
+
+            default:
+                break;
+        }
+    }
+    
     return (
         <div>
-            Pages
+            {renderPage()}
         </div>
     )
 }
