f65b2d2788a5df22d9887813c7be45f3434131b8
Rianna Castro <riannaxcastro@gmail.com>
2021-12-02 20:40:39 -1000

    lists rendering on page

--- src/components/home/HomeContainer.js
+++ src/components/home/HomeContainer.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react'
 
 export default class HomeContainer extends Component {
 
+    state = {
+        lists: []
+    }
+
     componentDidMount() {
         fetch('http://localhost:3001/lists')
         .then(r => r.json())
-        .then(data => console.log(data))
+        .then(data => this.setState({
+            lists: data
+        }))
     }
 
     render() {
         return (
             <div>
                 <h1>Home working</h1>
+                <ul>
+                    {this.state.lists.map(l => <li>{l.title}< br/> {l.tasks}</li>)}
+                </ul>
             </div>
         )
     }
